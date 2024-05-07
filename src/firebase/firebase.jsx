d77cf1ddeba5b5e5b53244f399e8ab12ef7496f8
Ombhesania <bhesaniaom@gmail.com>
2024-05-07 19:10:03 +0530

    removed the folder

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -14,6 +14,14 @@ const firebaseConfig = {
   messagingSenderId: "916389254549",
   appId: "1:916389254549:web:dffb86eec8dc5abc86929c",
 };
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
